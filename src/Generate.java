73d0f89d71bb98b75e8666e80ecb53bb575f10c6
alex99q <alex99q@abv.bg>
2017-05-05 23:47:10 +0300

    Fixed some stuff in class Generate.

--- src/Generate.java
+++ src/Generate.java
@@ -9,7 +9,9 @@ public class Generate {
                             DataInput.DMZ_Router +
                             DataInput.wifiRouter;
 
-        int pc = DataInput.workspaces + DataInput.wifiWorkspaces;
+        int pc = DataInput.workspaces;
+
+        int wirelessDevices = DataInput.wifiWorkspaces;
 
         // Add routers
         List<String> devices = new ArrayList<>();
@@ -17,8 +19,12 @@ public class Generate {
             devices.add("R".concat(Integer.toString(i)));
 
         // Add PC's
-        for (int i = 1; i < pc; i++) {
+        for (int i = 1; i <= pc; i++) {
             devices.add("PC".concat(Integer.toString(i)));
+        }
+
+        for (int i = 1; i <= wirelessDevices; i++){
+            devices.add("WifiDevice".concat(Integer.toString(i)));
         }
 
         return devices;
@@ -26,22 +32,29 @@ public class Generate {
 
     public List<String> Interfaces() {
         int numberOfDepartmentRouters = DataInput.departmentsOnFirstFloor + DataInput.departmentsOnSecondFloor;
-
-        // "1" is the "G0/0" interface
-        int numberOfWifiInterfaces = DataInput.wifiRouter * DataInput.wifiWorkspaces + 1;
+        int numberOfWifiInterfaces = DataInput.wifiRouter * DataInput.wifiWorkspaces;
 
         List<String> interfaces = new ArrayList<>();
 
+        //Generates DMZ_Router's interface
         String DMZ_RouterInterface = "G0/1";
         interfaces.add(DMZ_RouterInterface);
 
+        //Generates Department Router interfaces
         for (int i = 0; i < numberOfDepartmentRouters; i++){
             interfaces.add("G0/0");
             interfaces.add("G0/1");
         }
 
+        //Generates wifi interfaces
+        interfaces.add("F0/0");
         for (int i = 0; i < numberOfWifiInterfaces; i++){
-            interfaces.add("G0/".concat(Integer.toString(i)));
+            interfaces.add("wireless-".concat(Integer.toString(i)));
+        }
+
+        //Generates PC interfaces
+        for (int i = 0; i < DataInput.workspaces; i++){
+            interfaces.add("Fa0");
         }
 
         return interfaces;
